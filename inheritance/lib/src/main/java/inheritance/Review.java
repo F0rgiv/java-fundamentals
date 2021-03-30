c54b300085532457aaa0c25e413b77b2ca0cfd27
James Mansour <jamestmansour@outlook.com>
2021-03-30 15:43:45 -0700

    Adds the ability for a review to have a movie if it's on a theater and ReviewTest.

--- inheritance/lib/src/main/java/inheritance/Review.java
+++ inheritance/lib/src/main/java/inheritance/Review.java
@@ -5,17 +5,33 @@ public class Review {
     String author;
     float rating; //0-5
     Place place;
+    String movie;
 
     public Review(String body, String author, int rating, Place place) {
-        //validate that the rating is properly set.
-        if (rating > 5) rating = 5;
-        if (rating < 0) rating = 0;
+        rating = getRating(rating);
 
         this.body = body;
         this.author = author;
         this.rating = rating;
         this.place = place;
         this.place.addReview(this);
+    }
+
+    private int getRating(int rating) {
+        if (rating > 5) rating = 5;
+        if (rating < 0) rating = 0;
+        return rating;
+    }
+
+    public Review(String body, String author, int rating, Place place, String movie) throws Exception {
+        if (!(place instanceof Theater)) throw new Exception("You can only specify a movie at the Theater!");
+        rating = getRating(rating);
+
+        this.body = body;
+        this.author = author;
+        this.rating = rating;
+        this.place = place;
+        this.movie = movie;
     }
 
     @Override
